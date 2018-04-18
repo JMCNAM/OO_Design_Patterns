3aea085c6536f212d9093d04b4312e3edb17f88b
James McNamara <32990455+JMCNAM@users.noreply.github.com>
2018-04-18 15:36:05 +0100

    Save and Load methods added to Document
    - Both take filename as user input WITHOUT .txt
    - Two documents available for testing load.
    - Need to edit output for GUI implementation

--- src/scanner/Document.java
+++ src/scanner/Document.java
@@ -1,31 +1,76 @@
 package scanner;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
+import scanner.GlyphFactory.Glyph;
 
 public class Document {
-	
-	// I want you to use a data structure other than
-	// an array, arraylist or hashmap to store the glyph data
-	// look at the collections framework and decide what might be appropriate
-	
+	// Linked list needs to be generic.
 	// Linked List.
-	LinkedList<Glyph> doc_list = new LinkedList<>();
-	
+	static LinkedList<Glyph> doc_list = new LinkedList<>();
+	// Outer-Inner instantiation.
+	// Add Glyph to list
 	public void in (Glyph glyph){
 		// System.out.println("glyph into document...");
 		doc_list.add(glyph);
-	}
-	
-	@Override
+	}	
+	// Document toString.
 	public String toString(){
 		// System.out.println("document toString...");
-		String s = "";
-		
+		String s = "";	
 		for(Glyph g: doc_list){
 			s += g.getCharacter();
 		}	
-		return s;
+		return s;		
+	}	
+	public void saveDocument(String filename) {
+		
+		try {		
+			FileWriter fileWriter = new FileWriter(filename+".txt");
+			BufferedWriter writer = new BufferedWriter(fileWriter);
+			
+			writer.write("Document:\n\n");
+			
+			for(Glyph g: doc_list) {
+				writer.write(g.getCharacter());
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
+	public void loadDocument(String filename) throws FileNotFoundException {
+		
+		File docFile = new File(filename+".txt");	
+		GlyphFactory gf = GlyphFactory.getInstance();
+		
+		try {
+			FileReader fileReader = new FileReader(docFile);
+			BufferedReader reader = new BufferedReader(fileReader);		
+			String line = null;
+			
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				for(Character c: line.toCharArray()) {
+					Glyph g = gf.getGlyph(c);
+					this.in(g);
+				}	
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void clearDocument() {
+		doc_list = new LinkedList<>();
+	}
 }
--- src/scanner/GlyphFactory.java
+++ src/scanner/GlyphFactory.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 // A flyweight class implemented as a singleton
 public class GlyphFactory {
 	
+	// Private instance.
 	private static GlyphFactory instance;
-	
 	// Private constructor for Singleton.	
 	private GlyphFactory() {
 	}
@@ -20,21 +20,36 @@ public class GlyphFactory {
 		return instance;
 	}
 	
-
-	// Linked list for Flyweight.
-	// LinkedList<Glyph> l = new LinkedList<>();	
-	
+	// HashMap for Flyweight.
 	HashMap<Character, Glyph> glyph_pool = new HashMap<Character, Glyph>();
-	
 	// getGlyph.
 	public Glyph getGlyph(Character c) {
 		// System.out.println("getting glyph...");
-		Glyph glph = glyph_pool.get(c);	
-			
+		Glyph glph = glyph_pool.get(c);				
 		if(glph == null) {
 			glph = new Glyph(c);
 		}
 		
 		return glph;
 	}
+	public class Glyph {	
+		private Glyph g;
+		// Character Attribute.
+		private char chr;
+		// Constructor.
+		private Glyph(Character c) {
+			this.chr = c;
+		}
+		// Getter.
+		public char getCharacter() {
+			return chr;
+		}
+		public Glyph returnGlyph() {
+			return g;
+		}
+	}
 }
+
+
+
+
